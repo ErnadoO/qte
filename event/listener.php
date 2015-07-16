3ac4599399a40d5fc0fb2adc8b890c859ccd6fff
ABDev <abdev@free.fr>
2015-07-16 14:09:28 +0200

    Allows setting attributes in new topics and display in viewforum

--- event/listener.php
+++ event/listener.php
@@ -28,6 +28,9 @@ class listener implements EventSubscriberInterface
 	/** @var \phpbb\template\template */
 	protected $template;
 
+	/** @var \phpbb\user */
+	protected $user;
+
 	/** @var string */
 	protected $root_path;
 
@@ -40,12 +43,13 @@ class listener implements EventSubscriberInterface
 	/** @var string */
 	protected $table_prefix;
 
-	public function __construct(\phpbb\request\request $request, \phpbb\cache\driver\driver_interface $cache, \phpbb\db\driver\driver_interface $db, \phpbb\template\template $template, \abdev\qte\qte $qte, $root_path, $php_ext, $table_prefix)
+	public function __construct(\phpbb\request\request $request, \phpbb\cache\driver\driver_interface $cache, \phpbb\db\driver\driver_interface $db, \phpbb\template\template $template, \phpbb\user $user, \abdev\qte\qte $qte, $root_path, $php_ext, $table_prefix)
 	{
 		$this->request = $request;
 		$this->cache = $cache;
 		$this->db = $db;
 		$this->template = $template;
+		$this->user = $user;
 		$this->qte = $qte;
 
 		$this->root_path = $root_path;
@@ -63,10 +67,17 @@ class listener implements EventSubscriberInterface
 			'core.acp_manage_forums_validate_data' => 'acp_manage_forums_validate_data_complement',
 			'core.acp_manage_forums_update_data_after' => 'acp_manage_forums_update_data_after_complement',
 
+			'core.mcp_topic_review_modify_row' => 'mcp_topic_review_modify_row_complement',
+
+			'core.posting_modify_message_text' => 'posting_modify_message_text_complement',
+			'core.posting_modify_submit_post_before' => 'posting_modify_submit_post_before_complement',
+			'core.posting_modify_template_vars' => 'posting_modify_template_vars_complement',
+			'core.viewforum_topic_row_after' => 'viewforum_topic_row_after_complement',
 			'core.viewtopic_assign_template_vars_before' => 'viewtopic_assign_template_vars_before_complement',
 			'core.viewtopic_modify_post_row' => 'viewtopic_modify_post_row_complement',
 			'core.viewtopic_modify_page_title' => 'viewtopic_modify_page_title_complement',
 
+			'core.submit_post_modify_sql_data' => 'submit_post_modify_sql_data_complement',
 			'core.delete_user_after' => 'delete_user_attributes',
 		);
 	}
@@ -172,6 +183,117 @@ class listener implements EventSubscriberInterface
 		}
 	}
 
+	public function mcp_topic_review_modify_row_complement($event)
+	{
+		if (!empty($event['topic_info']['topic_attr_id']) && !$event['current_row_number'])
+		{
+			$post_row = $event['post_row'];
+			$post_row += array('TOPIC_ATTRIBUTE' => $this->qte->attr_display($event['topic_info']['topic_attr_id'], $event['topic_info']['topic_attr_user'], $event['topic_info']['topic_attr_time']));
+			$event['post_row'] = $post_row;
+		}
+	}
+
+	public function posting_modify_message_text_complement($event)
+	{
+		$attr_id = $this->request->variable('attr_id', 0);
+		if ($attr_id != -2)
+		{
+			if (!empty($event['post_data']['topic_attr_id']))
+			{
+				if (empty($attr_id))
+				{
+					$post_data = $event['post_data'];
+					$post_data['attr_id'] = $event['post_data']['topic_attr_id'];
+					$event['post_data'] = $post_data;
+				}
+			}
+		}
+	}
+
+	public function posting_modify_submit_post_before_complement($event)
+	{
+		$attr_id = (($event['mode'] == 'post') && !empty($event['post_data']['default_attr'])) ? $event['post_data']['default_attr'] : $event['post_data']['attr_id'];
+
+		$data = $event['data'];
+		$data += array('attr_id' => (int) $attr_id);
+		$event['data'] = $data;
+	}
+
+	public function posting_modify_template_vars_complement($event)
+	{
+		$current_time = time();
+
+		if (!empty($event['post_data']['attr_id']) && ($event['post_data']['attr_id'] != -1))
+		{
+			$this->qte->get_users_by_user_id($this->user->data['user_id']);
+			$this->template->assign_vars(array(
+				'S_TOPIC_ATTR' => true,
+				'TOPIC_ATTRIBUTE' => $this->qte->attr_display($event['post_data']['attr_id'], $this->user->data['user_id'], $current_time),
+			));
+		}
+
+		if ($event['mode'] == 'post' || ($event['mode'] == 'edit' && $event['post_id'] == $event['post_data']['topic_first_post_id']))
+		{
+			$topic_attribute = $this->request->variable('attr_id', 0);
+			if (!$event['preview'])
+			{
+				if (!empty($event['post_data']['topic_attr_id']))
+				{
+					$topic_attribute = $event['post_data']['topic_attr_id'];
+				}
+			}
+			else
+			{
+				$topic_attribute = $this->request->variable('attr_id', 0);
+			}
+
+			$hide_attr = unserialize(trim($event['post_data']['hide_attr']));
+			if ($hide_attr === false)
+			{
+				$hide_attr = array();
+			}
+
+			$this->qte->attr_select($event['forum_id'], $this->user->data['user_id'], (int) $topic_attribute, $hide_attr);
+
+			$this->template->assign_vars(array(
+				'S_POSTING' => true,
+				'IS_AUTHOR' => ($event['post_data']['poster_id'] == $this->user->data['user_id']),
+			));
+		}
+
+		if (($event['mode'] == 'edit') && isset($topic_attribute) && ($event['post_id'] == $event['post_data']['topic_first_post_id']))
+		{
+			if ($event['preview'])
+			{
+				if ($topic_attribute != $event['post_data']['topic_attr_id'])
+				{
+					$post_data['topic_attr_id'] = (int) $topic_attribute;
+					$post_data['topic_attr_user'] = (int) $this->user->data['user_id'];
+					$post_data['topic_attr_time'] = (string) $current_time;
+				}
+			}
+
+			if ($event['post_data']['topic_attr_id'] != -1)
+			{
+				$this->qte->get_users_by_topic_id(array($event['post_data']['topic_id']));
+				$this->template->assign_vars(array(
+					'S_TOPIC_ATTR' => true,
+					'TOPIC_ATTRIBUTE' => $this->qte->attr_display($event['post_data']['topic_attr_id'], $event['post_data']['topic_attr_user'], $event['post_data']['topic_attr_time']),
+				));
+			}
+		}
+	}
+
+	public function viewforum_topic_row_after_complement($event)
+	{
+		if (!empty($event['row']['topic_attr_id']))
+		{
+			$this->template->alter_block_array('topicrow', array(
+				'TOPIC_ATTRIBUTE' => $this->qte->attr_display($event['row']['topic_attr_id'], $event['row']['topic_attr_user'], $event['row']['topic_attr_time']),
+			), true, 'change');
+		}
+	}
+
 	public function viewtopic_assign_template_vars_before_complement($event)
 	{
 		// did the user apply an attribute ? so, let's save it !
@@ -191,7 +313,7 @@ class listener implements EventSubscriberInterface
 		$this->qte->attr_select($event['forum_id'], $event['topic_data']['topic_poster'], $event['topic_data']['topic_attr_id'], $hide_attr);
 
 		$tpl_ary = array('S_QTE_FORM' => append_sid("{$this->root_path}viewtopic.{$this->php_ext}", "f={$event['forum_id']}&amp;t={$event['topic_id']}"));
-		if (!empty($topic_data['topic_attr_id']))
+	if (!empty($topic_data['topic_attr_id']))
 		{
 			$this->qte->get_users_by_topic_id(array($event['topic_data']['topic_id']));
 			$tpl_ary += array(
@@ -217,6 +339,30 @@ class listener implements EventSubscriberInterface
 		$topic_data = $event['topic_data'];
 		$topic_data['topic_title'] = $attribute_title . ' ' . $event['topic_data']['topic_title'];
 		$event['topic_data'] = $topic_data;
+	}
+
+	public function submit_post_modify_sql_data_complement($event)
+	{
+		if (in_array($event['post_mode'], array('post', 'edit_topic', 'edit_first_post')))
+		{
+			$attr_id = isset($event['data']['attr_id']) ? $event['data']['attr_id'] : -1;
+
+			if ($attr_id != -2)
+			{
+				if ($attr_id == -1)
+				{
+					$event['sql_data'][TOPICS_TABLE]['sql'] += array('topic_attr_id' => 0, 'topic_attr_user' => 0, 'topic_attr_time' => 0);
+				}
+				else
+				{
+					$event['sql_data'][TOPICS_TABLE]['sql'] += array(
+						'topic_attr_id' => $attr_id,
+						'topic_attr_user' => (int) $this->user->data['user_id'],
+						'topic_attr_time' => time(),
+					);
+				}
+			}
+		}
 	}
 
 	public function delete_user_attributes($event)
