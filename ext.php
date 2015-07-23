dcdbe5534e39d58e6eb9531752a37280b4f3b7b7
Zoddo <zoddo.ino@gmail.com>
2015-07-23 22:16:54 +0200

    Don't allow to enable QTE if phpBB version isn't compatible

---
+++ ext.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * @package Quick Title Edition Extension
+ * @copyright (c) 2015 ABDev
+ * @copyright (c) 2015 PastisD
+ * @copyright (c) 2015 Geolim4 <http://geolim4.com>
+ * @copyright (c) 2015 Zoddo <zoddo.ino@gmail.com>
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace abdev\qte;
+
+class ext extends \phpbb\extension\base
+{
+    public function is_enableable()
+    {
+        $config = $this->container->get('config');
+        return phpbb_version_compare($config['version'], '3.1.3-RC1', '>=');
+    }
+}
