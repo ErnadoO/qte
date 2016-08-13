5bb186d4a95271642f9ebcdd5aceae37df94599c
Erwan Nader <ernadoo@gmail.com>
2016-08-13 18:56:55 +0200

    Add search attribute feature

---
+++ search/fulltext_attribute.php
@@ -0,0 +1,189 @@
+<?php
+/**
+*
+* phpBB Directory extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 ErnadoO <http://www.phpbb-services.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace ernadoo\qte\search;
+
+class fulltext_attribute extends \phpbb\search\base
+{
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\config\config				$config Config object
+	* @param \phpbb\db\driver\driver_interface	$db		Database object
+	*/
+	public function __construct(\phpbb\config\config $config, \phpbb\db\driver\driver_interface $db)
+	{
+		$this->config	= $config;
+		$this->db		= $db;
+	}
+
+	/**
+	* Performs a search on keywords depending on display specific params. You have to run split_keywords() first
+	*
+	* @param	array		$keywords_ary		contains each words to search
+	* @param	string		$terms				is either 'all' (use query as entered, words without prefix should default to "have to be in field") or 'any' (ignore search query parts and just return all posts that contain any of the specified words)
+	* @param	array		$sort_by_sql		contains SQL code for the ORDER BY part of a query
+	* @param	string		$sort_key			is the key of $sort_by_sql for the selected sorting
+	* @param	string		$sort_dir			is either a or d representing ASC and DESC
+	* @param	string		$sort_days			specifies the maximum amount of days a post may be old
+	* @param	array		$ex_cid_ary			specifies an array of category ids which should not be searched
+	* @param	int			$cat_id				is set to 0 or a topic id, if it is not 0 then only posts in this topic should be searched
+	* @param	array		&$id_ary			passed by reference, to be filled with ids for the page specified by $start and $per_page, should be ordered
+	* @param	int			$start				indicates the first index of the page
+	* @param	int			$per_page			number of ids each page is supposed to contain
+	* @return	int								total number of results
+	*/
+	public function attribute_search($attribute_id, $type, $terms, $sort_by_sql, $sort_key, $sort_dir, $sort_days, $ex_fid_ary, $post_visibility, $topic_id, $author_ary, $author_name, &$id_ary, &$start, $per_page)
+	{
+		// generate a search_key from all the options to identify the results
+		$search_key_array = array(
+			$attribute_id,
+			$type,
+			'firstpost',
+			'',
+			'',
+			$sort_days,
+			$sort_key,
+			$topic_id,
+			implode(',', $ex_fid_ary),
+			$post_visibility,
+			implode(',', $author_ary),
+			$author_name,
+		);
+
+		$search_key = md5(implode('#', $search_key_array));
+
+		if ($start < 0)
+		{
+			$start = 0;
+		}
+
+		// try reading the results from cache
+		$result_count = 0;
+		if ($this->obtain_ids($search_key, $result_count, $id_ary, $start, $per_page, $sort_dir) == SEARCH_RESULT_IN_CACHE)
+		{
+			return $result_count;
+		}
+
+		$id_ary = array();
+
+		// Create some display specific sql strings
+		$sql_attribute	= 't.topic_attr_id = ' . (int) $attribute_id;
+		$sql_fora		= (sizeof($ex_fid_ary)) ? ' AND ' . $this->db->sql_in_set('p.forum_id', $ex_fid_ary, true) : '';
+		$sql_topic_id	= ($topic_id) ? ' AND p.topic_id = ' . (int) $topic_id : '';
+		$sql_time		= ($sort_days) ? ' AND p.post_time >= ' . (time() - ($sort_days * 86400)) : '';
+		$sql_firstpost	= ' AND p.post_id = t.topic_first_post_id';
+
+		// Build sql strings for sorting
+		$sql_sort = $sort_by_sql[$sort_key] . (($sort_dir == 'a') ? ' ASC' : ' DESC');
+		$sql_sort_table = $sql_sort_join = '';
+		switch ($sql_sort[0])
+		{
+			case 'u':
+				$sql_sort_table	= USERS_TABLE . ' u, ';
+				$sql_sort_join	= ($type == 'posts') ? ' AND u.user_id = p.poster_id ' : ' AND u.user_id = t.topic_poster ';
+			break;
+
+			case 'f':
+				$sql_sort_table	= FORUMS_TABLE . ' f, ';
+				$sql_sort_join	= ' AND f.forum_id = p.forum_id ';
+			break;
+		}
+
+		$m_approve_fid_sql = ' AND ' . $post_visibility;
+
+		// If the cache was completely empty count the results
+		$calc_results = ($result_count) ? '' : 'SQL_CALC_FOUND_ROWS ';
+
+			// Build the query for really selecting the post_ids
+		if ($type == 'posts')
+		{
+			$sql = "SELECT {$calc_results}p.post_id
+				FROM " . $sql_sort_table . POSTS_TABLE . ' p, ' . TOPICS_TABLE . ' t ' . "
+				WHERE $sql_attribute
+					$sql_topic_id
+					$sql_firstpost
+					$m_approve_fid_sql
+					$sql_fora
+					$sql_sort_join
+					$sql_time
+				ORDER BY $sql_sort";
+			$field = 'post_id';
+		}
+		else
+		{
+			$sql = "SELECT {$calc_results}t.topic_id
+				FROM " . $sql_sort_table . TOPICS_TABLE . ' t, ' . POSTS_TABLE . " p
+				WHERE $sql_attribute
+					$sql_topic_id
+					$sql_firstpost
+					$m_approve_fid_sql
+					$sql_fora
+					AND t.topic_id = p.topic_id
+					$sql_sort_join
+					$sql_time
+				GROUP BY t.topic_id
+				ORDER BY $sql_sort";
+			$field = 'topic_id';
+		}
+
+		// Only read one block of posts from the db and then cache it
+		$result = $this->db->sql_query_limit($sql, $this->config['search_block_size'], $start);
+
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			$id_ary[] = (int) $row[$field];
+		}
+		$this->db->sql_freeresult($result);
+
+		// retrieve the total result count if needed
+		if (!$result_count)
+		{
+			$sql_found_rows = 'SELECT FOUND_ROWS() as result_count';
+			$result = $this->db->sql_query($sql_found_rows);
+			$result_count = (int) $this->db->sql_fetchfield('result_count');
+			$this->db->sql_freeresult($result);
+
+			if (!$result_count)
+			{
+				return false;
+			}
+		}
+
+		if ($start >= $result_count)
+		{
+			$start = floor(($result_count - 1) / $per_page) * $per_page;
+
+			$result = $this->db->sql_query_limit($sql, $this->config['search_block_size'], $start);
+			while ($row = $this->db->sql_fetchrow($result))
+			{
+				$id_ary[] = (int) $row[$field];
+			}
+			$this->db->sql_freeresult($result);
+
+			$id_ary = array_unique($id_ary);
+		}
+
+		if (sizeof($id_ary))
+		{
+			$this->save_ids($search_key, '', $author_ary, $result_count, $id_ary, $start, $sort_dir);
+			$id_ary = array_slice($id_ary, 0, $per_page);
+
+			return $result_count;
+		}
+		return false;
+	}
+}
