dd0612971402b7b5cc1dff211fa341a7ee3f2d39
Erwan Nader <ErnadoO@users.noreply.github.com>
2016-09-27 11:11:04 +0200

    Update minimum requirement

--- ext.php
+++ ext.php
@@ -19,6 +19,6 @@ class ext extends \phpbb\extension\base
 	public function is_enableable()
 	{
 		$config = $this->container->get('config');
-		return phpbb_version_compare($config['version'], '3.1.3-RC1', '>=');
+		return phpbb_version_compare($config['version'], '3.1.10-RC1', '>=');
 	}
 }
