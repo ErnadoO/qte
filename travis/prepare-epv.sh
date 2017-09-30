c806ca31820a0646fa6feb9784538ebf7da14241
erwan nader <ernadoo@gmail.com>
2017-09-30 14:11:33 +0200

    Update epv tests

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -15,7 +15,7 @@ EPV=$1
 NOTESTS=$2
 TRAVIS_PHP_VERSION=$3
 
-if [ "$EPV" == "1" -a "$DB" == "mysqli" -a "$TRAVIS_PHP_VERSION" == "5.5" ]
+if [ "$EPV" == "1" -a "$DB" == "mysqli" -a "$TRAVIS_PHP_VERSION" == "5.6" ]
 then
 	cd phpBB
 	composer require phpbb/epv:dev-master --dev --no-interaction
